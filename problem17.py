8991791f428faf26af69d7fafae308456fe5cfbb
github username <harr.hughes@gmail.com>
2020-08-10 11:42:55 +0100

    problem 17 complete

--- problem17.py
+++ problem17.py
@@ -8,14 +8,36 @@
 def number_sequence_letter_total_up_to_n(n):
     total = 0
     for i in range(1, n+1):
+        print(i, calc_length_of_num(i))
         total += calc_length_of_num(i)
+    return total
 
 
 def calc_length_of_num(x):
-    key_num = {1: 3, 2: 3, 3: 5, 4: 4, 5: 4, 6: 3, 7: 5, 8: 5, 9: 4, 10: 3, 11: 7, 12: 6,
-               13: 8, 14: 8, 15: 7, 16: 7, 17: 9, 18: 8, 19: 8, 20: 6, 30: 6, 40: 6, 50: 5, 60: 5, 70: 7, 80: 6, 90: 6}
-    if x in key_num:
-        if not int(x/100) == 0:
-            if x == 100:
+    key_num = {1: 3, 2: 3, 3: 5, 4: 4, 5: 4, 6: 3, 7: 5, 8: 5, 9: 4, 10: 3, 11: 6, 12: 6,
+               13: 8, 14: 8, 15: 7, 16: 7, 17: 9, 18: 8, 19: 8, 20: 6, 30: 6, 40: 5, 50: 5, 60: 5, 70: 7, 80: 6, 90: 6, 1000: 11}
 
-                int(x/100)
+    if x == 0:
+        return 0
+
+    if x in key_num:
+        return key_num[x]
+
+    def calc_length_less_than_100(x):
+        if x == 0:
+            return x
+        return key_num[(int(x/10)*10)] + key_num[int(x % 10)]
+
+    if int(x/100) == 0:  # less than 100
+        return calc_length_less_than_100(x)
+    else:  # >= 100
+
+        if x % 100 == 0:  # exact divisor of 100
+            return key_num[int(x/100)] + 7  # x hundred
+        else:
+            # x hundred and x
+            return key_num[int(x/100)] + 10 + calc_length_of_num(x - 100*int(x/100))
+
+
+if __name__ == "__main__":
+    print(number_sequence_letter_total_up_to_n(1000))
