4315a48950c97a49bb331700a8c7d37f88a69934
github username <harr.hughes@gmail.com>
2020-08-10 12:58:43 +0100

    proj 19 halfway

--- problem19.py
+++ problem19.py
@@ -10,3 +10,28 @@
 # A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+
+def how_many_sundays(start_year, end_year, start_day):
+    non_leap_month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    leap_month_lengths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    [sunday_counter, start_of_month] = [0, start_day]
+    for year in range(start_year, end_year):
+        if ((not year % 100 == 0) and year % 4 == 0) or (year % 400 == 0):
+            for month in range(12):
+                start_of_month = (
+                    start_of_month + leap_month_lengths[month]) % 7
+                if start_of_month == 0:
+                    sunday_counter += 1
+        else:
+            for month in range(12):
+                start_of_month = (
+                    start_of_month + leap_month_lengths[month]) % 7
+                if start_of_month == 0:
+                    sunday_counter += 1
+        print(year)
+    return 0
+
+
+if __name__ == "__main__":
+    print(how_many_sundays(1901, 2000, 2))
