bf59cfec1613cbd87c42ee69702543a00c869767
github username <harr.hughes@gmail.com>
2020-09-14 23:58:48 +0100

    demo array

--- problem68.py
+++ problem68.py
@@ -9,3 +9,5 @@
 # By concatenating each group it is possible to form 9-digit strings; the maximum string for a 3-gon ring is 432621513.
 
 # Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What is the maximum 16-digit string for a "magic" 5-gon ring?
+
+demo_array = [4, 3, 2, 6, 2, 1, 5, 1, 3]
