127084304d624b1f23e7c2e37b770b3dfe34b450
github username <harr.hughes@gmail.com>
2020-09-16 23:34:27 +0100

    maximum potential gon

--- problem68.py
+++ problem68.py
@@ -10,18 +10,25 @@
 
 # Using the numbers 1 to 10, and depending on arrangements, it is possible to form 16- and 17-digit strings. What is the maximum 16-digit string for a "magic" 5-gon ring?
 
-demo_array = [4, 3, 2, 6, 2, 1, 5, 1, 3]
-demo_5_gon = [1, 2, 3, 4, 3, 5, 6, 5, 7, 8, 7, 9, 10, 9, 2]
+demo_5_gon = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# lines (by index) => (0, 9, 1), (2, 1, 3), (4, 3, 5), (6, 5, 7), (8, 7, 9)
 
-def confirm_3_gon(arr):
-    return True
-
+max_possible_order = [6, 9, 8, 7,
+max_outer_ring_clockwise = [6, 9, 8, 7, 10]
 
 def validate_5_gon(arr):
-    i = 0
+    i = arr[0] + arr[9] + arr[2]
     return i
 
 
+def gon_array_to_line(arr):
+    converter = [0, 9, 1, 2, 1, 3, 4, 3, 5, 6, 5, 7, 8, 7, 9]
+    line = ''
+    for index in converter:
+        line += str(arr[index])
+    return line
+
+
 if __name__ == "__main__":
-    print(validate_5_gon(demo_5_gon))
+    print(gon_array_to_line(demo_5_gon))
